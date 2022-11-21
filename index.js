66240d883d1e51d9261991699ec44a36d6f5cf64
Bob Oyier <boboyier92@gmail.com>
2022-11-21 18:51:53 +0300

    "completed the control flow assignment"

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(number) {
   // Write your code here!
+  if (number < 400) {
+    return "This one is on me!";
+  } else if (number > 2000) {
+    if (number > 2500) {
+      return "No can do.";
+    } else return "I will gladly take your thirty bucks.";
+  }
+}
+// console.log(scuberGreetingForFeet(2501));
+
+function ternaryCheckCity(city) {
+  // Write your code here!
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
+function switchOnCharmFromTip(tip) {
   // Write your code here!
-}
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+    default:
+      return "Bye.";
+      break;
+  }
 }
